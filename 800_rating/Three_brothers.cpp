06b09c38037a93db24a7a08fa88b854451c2a007
Topu <113087685+topuBhuyan@users.noreply.github.com>
2024-09-21 20:09:31 +0600

    Add files via upload

---
+++ 800_rating/Three_brothers.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int totalSum = 6;
+    int a , b, c; cin >> a>> b;
+
+    int sum = a + b;
+
+    c = totalSum - sum;
+
+    cout << c <<'\n';
+
+    return 0;
+}
